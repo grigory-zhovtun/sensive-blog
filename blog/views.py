603a511b157fcd1497e08043868cb9ba33108ccb
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-10-10 22:11:23 +0500

    optimize queries for post detail view and add related_name for comments.

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -43,6 +57,13 @@ def index(request):
         posts_count=Count('posts')
     ).order_by('-posts_count')[:5]
 
+    # context = {
+    #     'most_popular_posts': [
+    #         serialize_post_optimized(post) for post in most_popular_posts
+    #     ],
+    #     'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
+    #     'popular_tags': [serialize_post_optimized(tag) for tag in most_popular_tags],
+    # }
     context = {
         'most_popular_posts': [
             serialize_post(post) for post in most_popular_posts
@@ -54,8 +75,11 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
-    comments = Comment.objects.filter(post=post)
+    post = Post.objects.prefetch_related(
+        'tags', 'likes', 'author'
+    ).get(slug=slug)
+    comments = Comment.objects.filter(post=post).select_related('author')
+
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
@@ -90,7 +114,7 @@ def post_detail(request, slug):
         'post': serialized_post,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
     }
     return render(request, 'post-details.html', context)
