f05b49133fb858f2fa4c430585bbc9c74ab8d146
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-10-17 09:31:41 +0500

    remove old commented code

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-# admin.site.register(Post)
-# admin.site.register(Tag)
-# admin.site.register(Comment)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -18,8 +15,3 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['post', 'author', 'published_at']
-
-# @admin.register(Admin)
-# class AdminAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'telegram_id', 'is_active']
-#     search_fields = ['name', 'telegram_id']
